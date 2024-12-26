d203ec478f18d46679edc9e54dba3f7e995ee0f5
hajar ouaslam <hajarouaslam@hajars-MacBook-Pro.local>
2024-12-26 15:35:12 +0100

    checking user authentication

--- requirements/game/backend/api/serializer.py
+++ requirements/game/backend/api/serializer.py
@@ -93,11 +93,7 @@ class PlayerSerializer( serializers.ModelSerializer ):
     def get_general_details( self, obj ):
         return {
             "total_points": obj.total_points,
-<<<<<<< HEAD
-            "total_games": obj.total_games,\
-=======
             "total_games": obj.total_games,
->>>>>>> refs/remotes/origin/game-backend
             "rank": self.get_rank( obj ),
             "level": obj.level
         }
--- requirements/game/backend/api/views.py
+++ requirements/game/backend/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .serializer import PlayerSerializer, PlayerRankSerializer
 from game.models import Player
 from django.contrib.auth.models import User
-
+import requests
 
 class PlayerDetailView( generics.RetrieveAPIView ):
     lookup_field = 'userId'
@@ -13,10 +13,15 @@ class PlayerDetailView( generics.RetrieveAPIView ):
 
     def retrieve( self, request, *args, **kwargs ):
         userId = kwargs.get( 'userId' )
+        response = requests.get( f'http://user_management:8000/auth/users/{userId}' )
+        if response.status_code != 200:
+            return Response( data={'detail': 'User not authenticated'}, status=401)
+        user_info = response.json(  )
+        
         user = User.objects.filter( id=userId).first(  )
         if not user:
             return Response( data={'detail': 'no userId was provided'}, status=404)
-        Player.objects.get_or_create( userId=user.id )
+        Player.objects.get_or_create( userId=user.id, username=user_info.get( 'username' ) )
         return super().retrieve(request, *args, **kwargs)
 
 class leaderBoardView( generics.ListAPIView ):
