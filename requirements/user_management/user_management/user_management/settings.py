ee4171b5e3a548212aa05a88f5a36ab731ea5282
hajar ouaslam <hajarouaslam@hajars-MacBook-Pro.local>
2025-01-08 16:40:25 +0100

    still trying

--- requirements/user_management/user_management/user_management/settings.py
+++ requirements/user_management/user_management/user_management/settings.py
@@ -198,4 +198,4 @@ INTRA_AUTH_URL = os.environ.get('INTRA_AUTH_URL')
 INTRA_TOKEN_URI = os.environ.get('INTRA_TOKEN_URI')
 USER_INFO_URI = os.environ.get('USER_INFO_URI')
 
-NEW_PLAYER_URL = os.environ.get('NEW_PLAYER_URL')
+NEW_PLAYER_URL = "http://game:8001/api/game/new_player" #os.environ.get('NEW_PLAYER_URL')
