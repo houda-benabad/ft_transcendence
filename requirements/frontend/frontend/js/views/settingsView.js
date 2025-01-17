2411e41b9c5ee084a4e472e759c6f762e31e9af4
User <hobenaba1337@gmail.com>
2025-01-17 19:08:15 +0100

    working on the settings ones

--- requirements/frontend/frontend/js/views/settingsView.js
+++ requirements/frontend/frontend/js/views/settingsView.js
@@ -12,8 +12,8 @@ export class settingsView extends HTMLElement
     {
        this.innerHTML = settingsTemplate.layout()
        this.querySelector('#settings-box1').innerHTML = settingsTemplate.settingsBox1()
-       this.querySelector('#settings-box2').innerHTML = settingsTemplate.settingsBox2()
-       this.querySelector('#settings-box3').innerHTML = settingsTemplate.settingsBox3()
+    //    this.querySelector('#settings-box2').innerHTML = settingsTemplate.settingsBox2()
+    //    this.querySelector('#settings-box3').innerHTML = settingsTemplate.settingsBox3()
 
     //    eventListeners.setAllByType(document.querySelectorAll('.anchor-tmp'), 'click')
     }
