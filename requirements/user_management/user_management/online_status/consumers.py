b1971b1fa3b816b3f93b95b5e40c76d35b367af3
Hind Benabad <hbenabad@e1r9p7.1337.ma>
2025-02-01 19:29:02 +0100

    finished the online_status handling with redis and switching from groups to from channel to channel better performance

--- requirements/user_management/user_management/online_status/consumers.py
+++ requirements/user_management/user_management/online_status/consumers.py
@@ -3,40 +3,64 @@ from friendship.models import Friend
 import json
 from asgiref.sync import async_to_sync
 from django.contrib.auth.models import AnonymousUser
+import redis
+from django.conf import settings
+
+REDIS_ONLINE_USERS = "online_users"
 
 class OnlineStatusConsumer(WebsocketConsumer):
+    redis_client = redis.redis(host=settings.REDIS_SERVER, port=settings.REDIS_PORT)
 
     def connect(self):
         
         if self.scope.get("user") != AnonymousUser():
             self.accept()
             self.user = self.scope.get("user")
-            self.group_name = user.username + "_friends"
-            async_to_sync(self.channel_layer.group_add(self.group_name, self.channel_name))
+            username = self.user.username
 
-            friends_qs = Friend.objects.friends(self.scope.get("user"))
-            for friend in friends_qs:
-                async_to_sync(self.channel_layer.group_add(friend.username + "_friends", self.channel_name))
+            self.redis_client.sadd(REDIS_ONLINE_USERS, self.user.username)
+            self.redis_client.sadd(f"{username}_channels", self.channel_name)
+            friends_qs = Friend.objects.friends(self.user)
+            friends_usernames = friends_qs.values_list("username", flat=True)
+            self._inform_friends_user_online_status(username, friends_usernames, "online")
+            online_friends = self._get_online_friends(friends_usernames)
+            self.send(text_data=json.dumps({"online_friends": online_friends}))
+    
+    def _inform_friends_user_online_status(self, username, friends_usernames, online_status):
 
-            async_to_sync(self.channel_layer.group_send(
-                self.group_name, {"type": "friend.status", "friend_username": self.scope.get("user").username, "status": "online"}
-            ))
-        
-        # when the group is created add it to redis and then before adding self.channel to other groups
-        # check if the user is already part of those groups  by checking group sets that were added in redis
-        # also using redis keep track of the users that are online
-        # so that when the user first logs in he sees the online status of the friends that connected before
-        
+        for friend_username in friends_usernames:
+
+            friend_channels = self.redis_client.smembers(f"{friend_username}_channels")
+
+            for friend_channel in friend_channels:
+                async_to_sync(self.channel_layer.send(
+                    friend_channel, 
+                    {
+                        "type": "friend.status",
+                        "friend_username": username,
+                        "status": status,
+                    }))
+    
+    def _get_online_friends(self, friends_usernames):
+
+        online_friends = []
+        for friend_username in friends_usernames:
+            if self.redis_client.sismembers(REDIS_ONLINE_USERS, friend_username) == -1:
+                online_friends.append(friend_username)
+            
+        return online_friends
+
     
     def disconnect(self, close_code):
 
-        if self.scope.get("user") != AnonymousUser():
-            friends_qs = Friend.objects.friends(self.user)
+        if self.user != AnonymousUser():
 
-            async_to_sync(self.channel_layer.group_send(
-                self.group_name, {"type": "friend.status", "friend_username": self.scope["user"].username, "status": "offline"}
-            ))
-    
+            self.redis_client.srem(REDIS_ONLINE_USERS, self.user.username)
+            self.redis_client.srem(f"{username}_channels", self.channel_name)
+            friends_qs = Friend.objects.friends(self.user)
+            friends_usernames = friends_qs.values_list("username", flat=True)
+            self._inform_friends_user_online_status(username, friends_usernames, "offline")
+
 
     def friend_status(self, event):
 
