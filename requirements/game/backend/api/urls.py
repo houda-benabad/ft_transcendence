41ac9aaaf86c6a86838e654ec015fb3859e749c8
hajar ouaslam <hajarouaslam@hajars-MacBook-Pro.local>
2025-02-03 16:44:41 +0100

    added view for update player

--- requirements/game/backend/api/urls.py
+++ requirements/game/backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import PlayerDetailView, leaderBoardView,  NewPlayerView
+from .views import PlayerDetailView, leaderBoardView,  NewPlayerView, UpdatePlayerView
 
 urlpatterns = [
     path( 'game/player_info/<int:userId>', PlayerDetailView.as_view(  ), name="PlayerInfo"),
     path( 'game/player_info/me', PlayerDetailView.as_view(  ), name="MeInfo"),
     path( 'game/new_player', NewPlayerView.as_view(  ), name="NewPlayer"),
+    path( 'game/update_player', UpdatePlayerView.as_view(  ), name="UpdatePlayer"),
     path( 'game/leaderboard/', leaderBoardView.as_view(  ) , name="LeaderBoard"),
     
 ]
